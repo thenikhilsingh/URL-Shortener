335661312eae2551eb3dea8c8a910aaf4d4e4cb8
thenikhilsingh <nikhilsingh26042004@gmail.com>
2025-10-02 19:59:24 +0530

    done

--- schema/url_schema.js
+++ schema/url_schema.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const urlSchema = mongoose.Schema({
   id: { type: Number },
   url: { type: String },
-  shortCode: { type: String },
+  shortCode: { type: String }, 
 });
 
 export const urls = mongoose.model("url", urlSchema);
